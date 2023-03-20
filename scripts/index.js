15a9014723c1edd00bce8b43d60ddc8e22f3d078
Wowikstar <admin@wowik.ru>
2023-03-20 21:06:28 +0700

    final commit

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const popup = document.querySelector('.popup');
+const formElement = popup.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__input_name_info');
+const jobInput = formElement.querySelector('.popup__input_job_info');
+const popupButtonClose = popup.querySelector('.popup__button-close');
+
+const profileBox = document.querySelector('.profile');
+const profileEdit = profileBox.querySelector('.profile__button-edit');
+const profileTitle = profileBox.querySelector('.profile-info__title');
+const profileSubtitle = profileBox.querySelector('.profile-info__subtitle');
+
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+}
+
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup();
+}
+
+profileEdit.addEventListener('click', openPopup);
+popupButtonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
+
+
